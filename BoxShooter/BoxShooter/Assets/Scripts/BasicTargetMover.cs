86bd6dc204470db6e0b313db46deefb52743dbb8
Aleyenda <drizzketo@gmail.com>
2015-10-24 12:59:27 +0200

    Adding BoxShotter project
    Integrating project to Git hub

---
+++ BoxShooter/BoxShooter/Assets/Scripts/BasicTargetMover.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicTargetMover : MonoBehaviour
+{
+
+    public float spinSpeed = 180.0f;
+    public float motionMagnitude = 0.1f;
+
+    public bool doSpin = true;
+    public bool doMotion = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (doSpin)
+        {
+            this.gameObject.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+        }
+
+        if (doMotion)
+        {
+            this.gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+        }
+    }
+}
